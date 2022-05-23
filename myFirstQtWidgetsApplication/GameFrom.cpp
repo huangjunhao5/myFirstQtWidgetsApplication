0518885232666acb4914763c310dd6ba2b95d1c2
huangjunhao5 <1814155869@qq.com>
2022-05-23 16:01:59 +0800

    实现点击

--- myFirstQtWidgetsApplication/GameFrom.cpp
+++ myFirstQtWidgetsApplication/GameFrom.cpp
@@ -1,9 +1,12 @@
+#pragma execution_character_set("utf-8")
 #include "GameFrom.h"
 #include <QtWidgets/QPushButton>
 #include<QtWidgets/QHBOxLayout>
 #include<QtWidgets/QVBOxLayout>
+#include<qmessagebox.h>
+#include<string>
 
-GameFrom::GameFrom(QWidget *parent)
+GameFrom::GameFrom(QWidget* parent)
 	: QWidget(parent)
 {
 	QPushButton** bt = new QPushButton * [16];
@@ -12,7 +15,13 @@ GameFrom::GameFrom(QWidget *parent)
 	for (int i = 0; i <= 15; i++) {
 		bt[i] = new QPushButton[16];
 		for (int j = 1; j <= 15; j++) {
+			QSizePolicy tmp = bt[i][j].sizePolicy();
+			tmp.setHorizontalPolicy(QSizePolicy::Minimum);
+			tmp.setVerticalPolicy(QSizePolicy::Minimum);
+			bt[i][j].setSizePolicy(tmp);
 			la[i].addWidget(&bt[i][j]);
+			connect(&bt[i][j], &QPushButton::clicked, this, [=]() {this->OnClick(i, j); });
+			//connect(&bt[i][j], SIGNAL(clicked()), this, SLOT(c1()));
 		}
 		va->addLayout(&la[i]);
 	}
@@ -20,10 +29,17 @@ GameFrom::GameFrom(QWidget *parent)
 	this->setLayout(va);
 }
 
-void OnClick(int x, int y) {
+void GameFrom::OnClick(int x, int y) {
 	mp[x][y] = 1;
+	QString s;
+	s = "";
+	s += QString::number(x, 10);
+	s += " ";
+	s += QString::number(y, 10);
+	QMessageBox::information(this, "标题", s, "我是文本");
 }
 
 GameFrom::~GameFrom()
 {
+
 }
